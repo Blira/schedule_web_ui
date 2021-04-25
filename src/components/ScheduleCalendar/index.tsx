6b9dc4b3586a0478e8ebcb335e407ad7c566d287
Bruno Lira <brunoelira@gmail.com>
2021-04-25 14:11:51 -0300

    feat: create function to disable busy days

--- src/components/ScheduleCalendar/index.tsx
+++ src/components/ScheduleCalendar/index.tsx
@@ -1,3 +1,4 @@
+import { Detail } from 'react-calendar';
 import { useSelectedDate } from '../../hooks/useSelectedDate'
 import './styles'
 import { Calendar } from './styles'
@@ -10,9 +11,38 @@ export function ScheduleCalendar() {
         setSelectedDate(newSelectedDate)
     }
 
+    interface DisableBusyDaysData {
+        activeStartDate: Date;
+        date: Date;
+        view: Detail;
+    }
+
+    interface BusyDate {
+        month: number;
+        days: number[]
+    }
+
+    const busyDates: BusyDate[] = [
+        {
+            month: 4,
+            days: [28]
+        }
+    ]
+
+    const disableBusyDays = ({ activeStartDate, date, view }: DisableBusyDaysData) => {
+        const currentMonth: BusyDate = busyDates.find(busyDate => busyDate.month === date.getMonth()) as BusyDate;
+        const disableMonth = !!currentMonth
+        const disableDay = currentMonth?.days.includes(date.getDate());
+        const disable = disableDay && disableMonth;
+        return disable;
+    }
+
+
     return (
         <Calendar
+            tileDisabled={disableBusyDays}
             minDate={new Date()}
+            showNeighboringMonth={false}
             onClickDay={handleClick}
         />
     )
